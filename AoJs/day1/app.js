c3b88968f56906ed083b9bad5564fef4447527a9
grassfinn <theharper@live.com>
2022-12-04 15:13:34 -0500

    logic and added setting button

--- AoJs/day1/app.js
+++ AoJs/day1/app.js
@@ -1,20 +1,88 @@
+const minutesEl = document.querySelector("input[value='15']");
+const secondsEl = document.querySelector("input[value='00']");
 
-const minutes = document.querySelector("input[value='15']")
+let seconds = 0;
+// let totalSeconds = minutes * 60;
+// let totalMinutes = totalSeconds / 60;
+let start = false
 
 
-let totalSeconds = (parseInt(minutes.value)) * 60
+function stopTimer(){
+  clearInterval(myInterval)
+}
 
+function toggleTimer(){
+  
+  start = !start
+  if (start) {
+    document.querySelector('.start').textContent = 'Stop'
+    startBtn.style.color = '#900A0A';
+    return
+  }
+  document.querySelector('.start').textContent = 'Start'
+  startBtn.style.color = '#09A65A'
+}
 
+const startBtn = document.querySelector('.start')
+startBtn.addEventListener('click', toggleTimer)
 
+document.querySelector('.settings').addEventListener('click', customTime)
 
-setInterval(() => {
-    let totalMinutes = (totalSeconds / 60);
-    let numberAfterDecimal = (totalMinutes % 1) * 100
-    let decimalToSeconds = (numberAfterDecimal * 60) / 100
+function customTime(){
+  start = false
+  let userInput = prompt("How many minutes would you like to add?")
+  // checks if userinput is not empty or is a string. if it is 
+  if (!userInput){
+    alert('Please insert a number')
+    return
+  }
+  console.log(parseInt(userInput))
+  minutesEl.value = parseInt(userInput)
+  secondsEl.value = `00`
+  
+}
+// get total amount of secs
+// convert into mins
+// have the remainder into secs
+
+function timer() {
+  let minutes = parseInt(minutesEl.value);
+  if (minutes === 0 && seconds === 0) {
+    return;
+  }
+
+  if (minutes > 0 && seconds === 0) {
+    minutesEl.value = minutes;
+    minutes--;
+  }
+
+  if (seconds > 0) {
+    secondsEl.value = seconds;
+    seconds--;
+  }
+
+  if (seconds < 10) {
+    secondsEl.value = `0${seconds}`;
+  }
+
+  if (seconds === 0) {
+    secondsEl.value = `0${seconds}`;
+    seconds = 59;
+  }
+  // totalSeconds--;
+  // seconds--;
+  console.log(seconds);
+  console.log(`${minutes}:${seconds}`);
+}
+
+const myInterval = setInterval(() => {
+    if (!start) return
+    // let numberAfterDecimal = (totalMinutes % 1) * 100
+    // let decimalToSeconds = Math.ceil((numberAfterDecimal * 60) / 100)
+    timer()
     
-    totalSeconds--
-    console.log('decimal', numberAfterDecimal)
-    console.log('seconds', decimalToSeconds)
-    console.log(totalMinutes)
-    console.log(totalSeconds)
-},1000)
+  // console.log('decimal', numberAfterDecimal)
+  // console.log('dec2Sec', decimalToSeconds)
+  // console.log(totalMinutes)
+  // console.log(totalSeconds)
+}, 1000);
